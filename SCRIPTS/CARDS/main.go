7117d93236c004a287a81e05bf3302d0f6b6724b
Santiago_Calle <cmsantiago32@gmail.com>
2023-10-10 23:00:09 -0500

    Primeras sesiones del curso

---
+++ SCRIPTS/CARDS/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var card string = "Ace of Spades" es una manera de nombrar la variable
+	card := "Ace of Spades"   // es otra manera de nombrar la variable igual de valida el ":=" es para declarar una variable NUEVA!
+	card = "Five of Diamonds" // de esta manera se puede cambiar el valor de la variable
+
+	fmt.Println(card)
+}
