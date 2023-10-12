7ec6da65f61ef31836c7f585fdad3768a327ace0
Santiago_Calle <cmsantiago32@gmail.com>
2023-10-12 01:53:11 -0500

    Start using loops and arrays (slice) at CARDS main

--- SCRIPTS/CARDS/main.go
+++ SCRIPTS/CARDS/main.go
@@ -4,8 +4,17 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of Spades" es una manera de nombrar la variable
-	card := "Ace of Spades"   // es otra manera de nombrar la variable igual de valida el ":=" es para declarar una variable NUEVA!
-	card = "Five of Diamonds" // de esta manera se puede cambiar el valor de la variable
+	card := newCard() // es otra manera de nombrar la variable igual de valida el ":=" es para declarar una variable NUEVA!
+	//card = "Five of Diamonds"  de esta manera se puede cambiar el valor de la variable
 
-	fmt.Println(card)
+	cards := []string{card, "Ace of diamonds"} // es un slice de strings
+	cards = append(cards, "Six of Spades")     // append es una funcion que agrega un elemento al slice
+
+	for i, carta := range cards { // el for range es para iterar sobre un slice
+		fmt.Println(i, carta)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
